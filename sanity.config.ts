407b48d3d326b8c4d10c56adc88e0ecbe98fdff7
Warfare-athen <athensdubey@gmail.com>
2025-02-01 19:48:49 +0530

    tgtg

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,7 +3,7 @@ import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { apiVersion } from "./sanity/env";
-import { schema } from "./sanity/schemaTypes";
+import { schema } from "./sanity/schemaTypes/index";
 import { structure } from "./sanity/structure";
 import { markdownSchema } from "sanity-plugin-markdown";
 
