c4e0fe1e15f70f56cbaee61e2299838872b45492
Warfare-athen <athensdubey@gmail.com>
2025-02-01 17:11:56 +0530

    upload

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,37 +1,20 @@
-// "use client";
-// import { visionTool } from "@sanity/vision";
-// import { defineConfig } from "sanity";
-// import { structureTool } from "sanity/structure";
-// import { apiVersion, dataset, projectId } from "./sanity/env";
-// import { schema } from "./sanity/schemaTypes";
-// import { structure } from "./sanity/structure";
-// import { markdownSchema } from "sanity-plugin-markdown";
-
-// export default defineConfig({
-//   basePath: "/studio",
-//   projectId,
-//   dataset,
-//   schema,
-//   plugins: [
-//     structureTool({ structure }),
-//     visionTool({ defaultApiVersion: apiVersion }),
-//     markdownSchema(),
-//   ],
-// });
-
-console.log("Loading Sanity configuration...");
+"use client";
+import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { visionTool } from "@sanity/vision";
-import { schema } from "@/sanity/schemaTypes/index";
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schemaTypes";
+import { structure } from "./sanity/structure";
+import { markdownSchema } from "sanity-plugin-markdown";
 
 export default defineConfig({
-  name: "default",
-  title: "My Sanity Studio",
-  projectId: "your-project-id",
-  dataset: "production",
-  plugins: [structureTool(), visionTool()],
-  schema: {
-    types: schema,
-  },
+  basePath: "/studio",
+  projectId,
+  dataset,
+  schema,
+  plugins: [
+    structureTool({ structure }),
+    visionTool({ defaultApiVersion: apiVersion }),
+    markdownSchema(),
+  ],
 });
